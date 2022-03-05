8e50a44a8eca43f0683e7407b7fdc988510a5082
Ikbal <mdiqbalhossain6463@gmail.com>
2022-03-05 12:39:11 +0600

    Jquery file

---
+++ index.js
@@ -0,0 +1,37 @@
+/*
+$("#p1").text("First para is changed by jquery")
+$("#p2").html("<b>Second para is bold</b>")
+$("#p3").append(" paragraph")
+$("#p4").prepend("paragraph ,")
+
+var newp  = $("<p></p>").text("This is new paragraph")
+$("#p3").before(newp)
+
+//const myattr = document.querySelector("a").getAttribute("href");
+//console.log(myattr);
+
+
+let myattribute = $("a").attr("href");
+console.log(myattribute);
+
+$("a").attr("href" ,"https://www.youtube.com") //set attribute
+*/
+
+$("h1").css("color" ,"red");
+$("h1").css("font-style" ,"italic");
+$("h1").css("font-size" ,"3rem");
+
+//$("h1").css( {"color":"green" , "font-size":"5rem"} ) //json formate
+
+$("h1").addClass("style1")
+
+
+
+
+
+
+
+
+
+
+
